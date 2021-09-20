f9fb20e085f4ff4523ec8beb15a4393a7b6ea52f
philippvlasov <pylyp.vlasov@valtech.com>
2021-09-20 22:00:54 +0300

    Add sass to the project

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import styles from './_scss/styles.scss';
+import 'bootstrap';
+import './_js/index';
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const globImporter = require('node-sass-glob-importer')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -13,6 +16,29 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              url: false
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                importer: globImporter()
+              }
+            }
+          },
+        ]
+      },
+      {
         test: /\.html$/i,
         loader: 'html-loader'
       },
@@ -21,13 +47,19 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'My App',
+      template: path.resolve(__dirname, "src/index.html"),
       filename: 'index.html',
-      template: 'src/index.html'
+      inject: true,
+      minify: false
     }),
     new BrowserSyncPlugin({
       host: 'localhost',
       port: 5006,
       server: { baseDir: ['dist'] }
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
+      ignoreOrder: false
+    }),
   ],
 };
